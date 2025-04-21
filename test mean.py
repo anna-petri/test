dbf7d0d3c6d4f0a12ff86c57a793f8f621a1b60d
anna-petri <anna-petri-2003@web.de>
2025-04-21 20:20:03 +0000

    add initial Python script and workspace configuration

--- test mean.py
+++ test mean.py
@@ -2,8 +2,9 @@ def mean(numbers):
     sum = 0
     for elem in numbers:
         sum += elem
-    sum = sum/len(numbers)
+    sum = sum / len(numbers)
     return sum
 
-numbers = [1,2,3]
-print("mean:",mean(numbers))
+
+numbers = [1, 2, 3]
+print("mean:", mean(numbers))
